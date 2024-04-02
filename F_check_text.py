f3291e64d5bcf27e3ab6926429db537cecc0572f
Vy Vo <vy.vo@intel.com>
2024-04-02 16:58:59 -0700

    Readability changes, with TODOs left to address. Added a top-level comment explaining purpose of script. Removed import * calls and specified functions instead. Removed unnecessary imports. Fixed variable naming that could be confused with global constants. Replaced 'video' with 'story'.

--- F_check_text.py
+++ F_check_text.py
@@ -1,11 +1,12 @@
+"""
+Loads the event segmentations for both story and recall. Exports an Excel .xlsx file that contains the original story
+event text that best matches the recall text.
+"""
+
 # loop over subjects
-import hypertools as hyp
 import numpy as np
-import tqdm
 from scipy.spatial.distance import cdist
 import more_itertools as mit
-# import brainiak.eventseg.event as event
-from scipy.stats import wasserstein_distance, pearsonr
 import os
 import sys
 try:
@@ -13,13 +14,9 @@ try:
 except:
     pass
 sys.path.append(os.getcwd())
-from sherlock_helpers.functions import *
-from sherlock_helpers.constants import *
-from sherlock_helpers.scoring import *
-import matplotlib.pyplot as plt
+from sherlock_helpers.functions import format_text, parse_windows_reg
+from sherlock_helpers.scoring import precise_matches_mat
 import pandas as pd
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 def get_story_text(corpus, bounds, winds):
     start_window, end_window = winds
@@ -34,31 +31,31 @@ def get_story_text(corpus, bounds, winds):
 
 
 # load HMM info
-DATA_DIR = 'result_models'
-TXT_DIR = 'result_text'
+data_dir = 'result_models'
+txt_dir = 'result_text'
 ############################3
 story_id='baseball'
 n_topics = 40
-video_size = 55
+story_size = 55
 step_size = 21
-subfolder = f'{story_id}_t{n_topics}_v{video_size}_r{video_size}_s{step_size}'
-#########################3#### if os.path.isfile(os.path.join(TXT_DIR,subfolder,'story.txt')):
+subfolder = f'{story_id}_t{n_topics}_v{story_size}_r{story_size}_s{step_size}'
+#########################3#### if os.path.isfile(os.path.join(txt_dir,subfolder,'story.txt')):
 #     return None
-video_events_times = np.load(os.path.join(DATA_DIR,subfolder,'video_event_times.npy'), allow_pickle=True)
-recall_events_times = np.load(os.path.join(DATA_DIR,subfolder,'recall_event_times.npy'), allow_pickle=True)
-video_events = np.load(os.path.join(DATA_DIR,subfolder,'video_events.npy'), allow_pickle=True)
-recall_events = np.load(os.path.join(DATA_DIR,subfolder,'recall_events.npy'), allow_pickle=True)
-# event_mappings = np.load(os.path.join(DATA_DIR,subfolder,'labels.npy'), allow_pickle=True)
-video_model, recall_models, recall_ids = np.load(os.path.join(os.getcwd(),DATA_DIR,subfolder+'.npy'),
+story_events_times = np.load(os.path.join(data_dir,subfolder,'video_event_times.npy'), allow_pickle=True)  # TODO: refactor instances of 'video*.file_ext'
+recall_events_times = np.load(os.path.join(data_dir,subfolder,'recall_event_times.npy'), allow_pickle=True)
+story_events = np.load(os.path.join(data_dir,subfolder,'video_events.npy'), allow_pickle=True)  # TODO: refactor instances of 'video*.file_ext'
+recall_events = np.load(os.path.join(data_dir,subfolder,'recall_events.npy'), allow_pickle=True)
+# event_mappings = np.load(os.path.join(data_dir,subfolder,'labels.npy'), allow_pickle=True)
+story_model, recall_models, recall_ids = np.load(os.path.join(os.getcwd(),data_dir,subfolder+'.npy'),
                                      allow_pickle=True)
 recall_ids = [os.path.join('./recall_transcript/recall_transcript_%s' % story_id,os.path.basename(i)) for i in recall_ids]
 
 # create folder to save
 # create folder to save data
-isExist = os.path.exists(os.path.join(TXT_DIR,subfolder))
+isExist = os.path.exists(os.path.join(txt_dir,subfolder))
 if not isExist:
     # Create a new directory because it does not exist
-    os.makedirs(os.path.join(TXT_DIR,subfolder))
+    os.makedirs(os.path.join(txt_dir,subfolder))
     print("The new directory is created!")
 # story - load original texts
 story_path = './%s_transcript.txt' % story_id
@@ -67,19 +64,19 @@ with open(story_path, encoding="utf8") as f:
 # story_fmt = format_text(story).replace('.','').split(' ')
 story_fmt = format_text(story).replace('\n', ' ').replace('.', '').strip()
 story_fmt = " ".join(story_fmt.split()).split(' ')
-video_size = int(subfolder.split('_')[2][1:])
+story_size = int(subfolder.split('_')[2][1:])
 step_size = int(subfolder.split('_')[4][1:])
-sub_story, bounds_story = parse_windows_reg(story_fmt, video_size, step_size)
+sub_story, bounds_story = parse_windows_reg(story_fmt, story_size, step_size)
 # check whether the windows match
-assert len(sub_story) == len(video_model)
+assert len(sub_story) == len(story_model)
 # create segmented story
-story_seg = [str(i+1)+'. '+get_story_text(story_fmt, bounds_story, video_events_times[i]) for i in range(len(video_events_times))]
+story_seg = [str(i+1)+'. '+get_story_text(story_fmt, bounds_story, story_events_times[i]) for i in range(len(story_events_times))]
 story_seg = '\n\n'.join(story_seg)
-with open(os.path.join(TXT_DIR,subfolder,'story.txt'), 'w') as f:
+with open(os.path.join(txt_dir,subfolder,'story.txt'), 'w') as f:
     f.write(story_seg)
 
 # recalls - original texts
-writer = pd.ExcelWriter(os.path.join(TXT_DIR, subfolder,'matched.xlsx'))
+writer = pd.ExcelWriter(os.path.join(txt_dir, subfolder,'matched.xlsx'))
 for idx, recall_id in enumerate(recall_ids):
     with open(recall_id) as f:
         recall = f.read()
@@ -89,22 +86,22 @@ for idx, recall_id in enumerate(recall_ids):
     recall_fmt = format_text(recall).replace('\n', ' ').replace('.', '').strip()
     recall_fmt = " ".join(recall_fmt.split()).split(' ')
     step_size = int(subfolder.split('_')[4][1:])
-    sub_recall, bounds_recall = parse_windows_reg(recall_fmt, video_size, step_size)
+    sub_recall, bounds_recall = parse_windows_reg(recall_fmt, story_size, step_size)
     assert len(sub_recall) == len(recall_models[idx])
     # create segmented recall
     recall_seg = [get_story_text(recall_fmt, bounds_recall, recall_events_times[idx][i])
                   for i in range(len(recall_events_times[idx]))]
     # save text
     recall_seg = '\n\n'.join(recall_seg)
-    with open(os.path.join(TXT_DIR, subfolder, os.path.basename(recall_ids[idx])[0:5]+'.txt'), 'w') as f:
+    with open(os.path.join(txt_dir, subfolder, os.path.basename(recall_ids[idx])[0:5]+'.txt'), 'w') as f:
         f.write(recall_seg)
 
     """
     save the matched text (using the precision matrix)
     """
-    corrmat = 1 - cdist(video_events, recall_events[idx], 'correlation')  # this is the correlation matrix
+    corrmat = 1 - cdist(story_events, recall_events[idx], 'correlation')  # this is the correlation matrix
     precise_mat = precise_matches_mat(corrmat,'recall')
-    story_segs = [get_story_text(story_fmt, bounds_story, video_events_times[i]) for i in np.where(precise_mat > 0)[0]]
+    story_segs = [get_story_text(story_fmt, bounds_story, story_events_times[i]) for i in np.where(precise_mat > 0)[0]]
     # recall_segs = [get_story_text(recall_fmt, bounds_recall,(j,j+1)) for j in np.where(precise_mat>0)[1]]
     matched = pd.DataFrame(data=dict(story_id=np.where(precise_mat>0)[0],story_segs=story_segs,
                            recall_id=np.where(precise_mat>0)[1]))
@@ -117,7 +114,7 @@ for idx, recall_id in enumerate(recall_ids):
             s = list(seg)
             txt_recall = ' '.join(recall_seg.split('\n\n')[s[0]:(s[-1]+1)])
             final_matched.loc[n] = [key]+[get_story_text(story_fmt, bounds_story,
-                                                         video_events_times[key])]+[(s[0],s[-1]+1)]+[txt_recall]
+                                                         story_events_times[key])]+[(s[0],s[-1]+1)]+[txt_recall]
             n+=1
     # write dataframe to excel sheet
     final_matched.to_excel(writer, os.path.basename(recall_ids[idx])[0:5])
